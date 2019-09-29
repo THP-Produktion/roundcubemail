71fe86bb0f53ee542ae4efeb7e420858b5a50477
THP-Produktion <54328977+THP-Produktion@users.noreply.github.com>
2019-09-29 10:54:23 +0200

    Update config.inc.php

--- config/config.inc.php
+++ config/config.inc.php
@@ -64,3 +64,37 @@ if (filter_var(getenv('DEBUG'), FILTER_VALIDATE_BOOLEAN)) {
   $config['log_dir'] = '/roundcube/logs';
   $config['log_driver'] = "file";
 }
+
+// Enables messages cache. Only 'db' cache is supported.
+// This requires an IMAP server that supports QRESYNC and CONDSTORE
+// extensions (RFC7162). See synchronize() in program/lib/Roundcube/rcube_imap_cache.php
+// for further info, or if you experience syncing problems.
+$config['imap_cache'] = 'db';
+$config['messages_cache'] = true;
+// Lifetime of IMAP indexes cache. Possible units: s, m, h, d, w
+$config['imap_cache_ttl'] = '10d';
+// Lifetime of messages cache. Possible units: s, m, h, d, w
+$config['messages_cache_ttl'] = '10d';
+// Maximum cached message size in kilobytes.
+// Note: On MySQL this should be less than (max_allowed_packet - 30%)
+$config['messages_cache_threshold'] = 50;
+$config['display_product_info'] = 2;
+$config['max_message_size'] = '2G';
+$config['email_dns_check'] = true;
+$config['enable_spellcheck'] = false;
+$config['max_pagesize'] = 1000;
+
+$config['show_images'] = 1;
+$config['htmleditor'] = 4;
+$config['mail_read_time'] = 10;
+$config['logout_purge'] = true;
+$config['check_all_folders'] = true;
+
+
+// Default font for composed HTML message.
+// Supported values: Andale Mono, Arial, Arial Black, Book Antiqua, Courier New,
+// Georgia, Helvetica, Impact, Tahoma, Terminal, Times New Roman, Trebuchet MS, Verdana
+$config['default_font'] = 'Verdana';
+// Default font size for composed HTML message.
+// Supported sizes: 8pt, 10pt, 12pt, 14pt, 18pt, 24pt, 36pt
+$config['default_font_size'] = '10pt';
